486d1180b9c4463aabc83e45933f06c6c831370c
rswilkhu <rajwilkhu@gmail.com>
2013-09-17 19:27:00 +0100

    Added the initial ASP.NET web application project for Web API and a unit test project.

---
+++ src/kata.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kata.Web.WebApiApplication" Language="C#" %>
---
+++ src/kata.web/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿namespace Kata.Web
+{
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using Kata.Web.Application.Start;
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
