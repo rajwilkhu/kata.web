a023d2f5fea10099ac4493b8bebd7b528654fe82
rswilkhu <rajwilkhu@gmail.com>
2013-09-24 21:47:23 +0100

    Added a project for unit testing the client.

---
+++ tests.unit/kata.web.client.tests.unit/Tests/Test.js
@@ -0,0 +1,15 @@
+/// <reference path="../Scripts/typings/jasmine/jasmine.d.ts" />
+describe("AnIntroductory", function () {
+    it("DummyTest", function () {
+        // Fixture setup
+        var sut = 1;
+
+        // Exercise system
+        var expectedNumber = sut;
+
+        // Verify outcome
+        expect(1).toBe(expectedNumber);
+        // Teardown
+    });
+});
+//# sourceMappingURL=Test.js.map
